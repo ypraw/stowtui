945f3e64b89fd1c7397a08aa30b5dea2232fbd3b
ypraw <dyo.ndutz@gmail.com>
2021-03-30 16:46:45 +0700

    ref(stowtui_core) : refactor and added docstring

--- stowgui/stowtui_core.py
+++ stowgui/stowtui_core.py
@@ -1,3 +1,19 @@
+"""
+---------
+Stow TUI Core
+---------
+
+StowTUI Core module.
+
+Define Classes for logical step to create symlink using stow packages.
+
+There are two static methods on this class:
+
+* ``getAllDir`` a getter for get list of directories of dotfiles.
+* ``stowExecute`` an executer for stow to create symlink use stow it self.
+
+"""
+
 import subprocess
 import time
 from os import listdir as workdir
@@ -10,16 +26,33 @@ class StowtuiCore:
 
     @staticmethod
     def getAllDir(path_to_dir: str):
+        """This is static method for get all list name on target directory
+
+        Args:
+            path_to_dir (str): [path to directory location, exp: ~/mydotfiles]
+
+        Returns:
+            (List[str]): [list directory name from target directory]
+        """
         get_only_directory: List[str] = [
             name for name in workdir(path_to_dir) if
             not name.startswith('.') and pth.isdir(pth.join(path_to_dir, name))
         ]
-        it = iter(get_only_directory)
-        list_of_tuple: List[Tuple[str, str]] = [(value, value) for value in it]
-        return list_of_tuple
+        # it = iter(get_only_directory)
+        # list_of_tuple: List[Tuple[str, str]] = [(value, value) for value in it]
+        return get_only_directory
 
     @staticmethod
     def stowExecute(dirsName: List[str], path_DIR):
+        """Static Method for executing stow function
+
+        Args:
+            dirsName (List[str]): [description]
+            path_DIR ([type]): [description]
+
+        Returns:
+            [type]: [Executing stow on shell]
+        """
         for keyName in dirsName:
             # if keyName[0] == 'neofetch':
             targetExecute = "cd {} && stow -R -t ~/ {}".format(
@@ -32,4 +65,5 @@ if __name__ == '__main__':
     path_DIR = '/home/ypraw/Programming/linux/configDotfiles'
     testing = StowtuiCore()
     TestingDir = testing.getAllDir(path_DIR)
-    print(testing.stowExecute(TestingDir, path_DIR))
+    print(TestingDir)
+    # print(testing.stowExecute(TestingDir, path_DIR))
