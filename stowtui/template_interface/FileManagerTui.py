717daa43cccc6169186733726a0b59048065cac9
ypraw <dyo.ndutz@gmail.com>
2021-04-02 02:39:54 +0700

    ref(stowAPP) : Added styling color and notes

--- stowtui/template_interface/FileManagerTui.py
+++ stowtui/template_interface/FileManagerTui.py
@@ -1,18 +1,19 @@
-"""
----------
-File Manager UI
----------
-
-File Manager module.
-
-UI Template for File Manager target directory and dotfiles directory.
-This UI will be rendered the first time the program is run.
-"""
-
 import npyscreen
+import os
+import sys
 
 
 class FileManagerTUI(npyscreen.ActionFormMinimal):
+    """
+    ---------
+    File Manager UI
+    ---------
+
+    File Manager module.
+
+    UI Template for File Manager target directory and dotfiles directory.
+    This UI will be rendered the first time the program is run.
+    """
 
     def __init__(self, *args, **keywords):
         super(FileManagerTUI, self).__init__(*args, **keywords)
@@ -21,8 +22,48 @@ class FileManagerTUI(npyscreen.ActionFormMinimal):
         self.edit()
         self.parentApp.setNextForm("directory_list")
 
+    @staticmethod
+    def exit(*args, **kwargs):
+        os.system('reset')
+        os.system('stty sane')
+        try:
+            sys.exit(0)
+        except SystemExit:  # pragma: no cover
+            os._exit(0)
+
     def create(self):
-        self.value = None
+        self.add_handlers({'^Q': FileManagerTUI.exit})
+
+        self.add(npyscreen.Textfield,
+                 value='NOTES:',
+                 editable=False,
+                 color='CONTROL')
+
+        self.nextrely += 1
+
+        self.add(
+            npyscreen.Textfield,
+            value=
+            'Target Directory is target to DIR (default is parent of stow dir) <ex, /home/$USER>',
+            editable=False,
+            color='CONTROL')
+
+        self.add(
+            npyscreen.Textfield,
+            value=
+            'Dotfiles Directory is Dotfiles config directory <ex, /home/$USER/dotfiles>',
+            editable=False,
+            color='CONTROL')
+
+        self.add(
+            npyscreen.Textfield,
+            value=
+            'set Dotfiles Directory path returned list of all directories config <ex, /home/$USER/dotfiles/zsh>',
+            editable=False,
+            color='CONTROL')
+
+        self.nextrely += 3
+
         self.target_directory = self.add(npyscreen.TitleFilenameCombo,
                                          name="Target Directory:",
                                          label=True)
