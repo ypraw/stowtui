8c8e5492343e0a13afe94360982cb7a3d093b87e
ypraw <dyo.ndutz@gmail.com>
2021-04-07 17:27:00 +0700

    REF (DOTFILES UI) : Added handler if dotfiles path not have child folders

--- stowtui/template_interface/DotfilesDirectories.py
+++ stowtui/template_interface/DotfilesDirectories.py
@@ -19,9 +19,8 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
     Dotfiles Directory
 
     """
-    CANCEL_BUTTON_TEXT = 'Previous Menu'
 
-    resCheckbox: Dict[str, str] = {}
+    CANCEL_BUTTON_TEXT = 'Previous Menu'
 
     def __init__(self, dotfiles_path=None, target_path=None, **kwargs):
         self.settings = locals()
@@ -36,7 +35,7 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
         os.system('stty sane')
         try:
             sys.exit(0)
-        except SystemExit:  # pragma: no cover
+        except SystemExit:    # pragma: no cover
             os._exit(0)
 
     def create(self):
@@ -53,11 +52,10 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
 
         self.nextrely += 1
 
-        self.add(
-            npyscreen.Textfield,
-            value='Make Sure that the folders is your dotfiles config',
-            editable=False,
-            color='CONTROL')
+        self.add(npyscreen.Textfield,
+                 value='Make Sure that the folders is your dotfiles config',
+                 editable=False,
+                 color='CONTROL')
 
         self.add(
             npyscreen.Textfield,
@@ -65,31 +63,27 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
             editable=False,
             color='CONTROL')
 
-        self.add(
-            npyscreen.Textfield,
-            value='-/zsh',
-            editable=False,
-            color='CONTROL')
+        self.add(npyscreen.Textfield,
+                 value='-/zsh',
+                 editable=False,
+                 color='CONTROL')
 
-        self.add(
-            npyscreen.Textfield,
-            value='\t - .zshrc',
-            editable=False,
-            color='CONTROL')
+        self.add(npyscreen.Textfield,
+                 value='\t - .zshrc',
+                 editable=False,
+                 color='CONTROL')
 
         self.nextrely += 1
 
-        self.add(
-            npyscreen.Textfield,
-            value='IF Not you can switch to previous menu',
-            editable=False,
-            color='CONTROL')
+        self.add(npyscreen.Textfield,
+                 value='IF Not you can switch to previous menu',
+                 editable=False,
+                 color='CONTROL')
 
-        self.add(
-            npyscreen.Textfield,
-            value='by pressing button cancel or use shortcut ^W',
-            editable=False,
-            color='CONTROL')
+        self.add(npyscreen.Textfield,
+                 value='by pressing button cancel or use shortcut ^W',
+                 editable=False,
+                 color='CONTROL')
 
         self.nextrely += 3
 
@@ -118,24 +112,31 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
 
         self.nextrely += 1
 
-        self.res = self.add(npyscreen.TitleMultiSelect,
-                            max_height=-2,
-                            name="List Directories Name",
-                            values=StowtuiCore.getAllDir(
-                                self.settings['dotfiles_path']),
-                            scroll_exit=True)
+        if StowtuiCore.getAllDir(self.settings['dotfiles_path']) is None:
+            npyscreen.notify_confirm(
+                'Target Dotfiles do not have any folders inside',
+                title='ERROR Dotfiles folder is null',
+                form_color='CRITICAL')
+        else:
+            self.res = self.add(npyscreen.TitleMultiSelect,
+                                max_height=-2,
+                                name="List Directories Name",
+                                values=StowtuiCore.getAllDir(
+                                    self.settings['dotfiles_path']),
+                                scroll_exit=True)
 
     def on_cancel(self, *args, **kwargs):
         self.parentApp.switchFormPrevious()
 
     def on_ok(self, **kwargs):
+
         def popup(thrd, title):
             """
             Start the thread and display a popup of the plugin being cloned
             until the thread is finished
             """
             thrd.start()
-            message_info = "Restoring..."
+            message_info = "loading..."
             npyscreen.notify_wait(message_info, title=title)
             while thrd.is_alive():
                 time.sleep(1)
@@ -146,11 +147,20 @@ class DotfilesDirectoriesList(npyscreen.ActionForm):
         selectable_values = self.res.get_selected_objects()
 
         if (selectable_values is None):
-            npyscreen.notify_confirm('Target Configs can not be null, please checklist at least one, or back to previous menu',title='ERROR Checklist is null', form_color='CRITICAL')
+            npyscreen.notify_confirm(
+                'Target Configs can not be null, please checklist at least one, or back to previous menu',
+                title='ERROR Checklist is null',
+                form_color='CRITICAL')
         else:
-            thrd = threading.Thread(
-                target=StowtuiCore.stowExecute, kwargs={'dirs_name': selectable_values, 'path_dir': target_dir, 'path_dotfiles': dotfiles_dir})
+            thrd = threading.Thread(target=StowtuiCore.stowExecute,
+                                    kwargs={
+                                        'dirs_name': selectable_values,
+                                        'path_dir': target_dir,
+                                        'path_dotfiles': dotfiles_dir
+                                    })
             popup(thrd, 'Restoring dotfiles')
-            npyscreen.notify_confirm('Done restored dotfiles with folder {}'.format(
-                str(selectable_values)[1:-1]), title='Restored Dotfiles')
+            npyscreen.notify_confirm(
+                'Done restored dotfiles with folder {}'.format(
+                    str(selectable_values)[1:-1]),
+                title='Restored Dotfiles')
             self.parentApp.switchFormPrevious()
