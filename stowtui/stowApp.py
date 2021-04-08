b0596f89b967d23596b84e5e53213d6ec8e335f3
ypraw <dyo.ndutz@gmail.com>
2021-04-08 17:14:10 +0700

    REF (stowAPP) : changed title name using helpers functions

--- stowtui/stowApp.py
+++ stowtui/stowApp.py
@@ -5,15 +5,20 @@ import npyscreen
 from typing import List
 
 from stowtui.template_interface.FileManagerTui import FileManagerTUI
+from stowtui.helpers.meta import Version
+from stowtui.helpers.meta import app_name
 
 
 class StowApp(npyscreen.NPSAppManaged):
 
     def onStart(self):
-        quit_s = '\t' * 4 + '^Q to quit'
+
+        quit_s = '\t' * 3 + '^Q to quit'
+        version = Version()
+        name = app_name().capitalize
 
         self.addForm(
             "MAIN",
             FileManagerTUI,
-            name='Stow ' + quit_s,
+            name=name + '\t' + version + quit_s,
         )
