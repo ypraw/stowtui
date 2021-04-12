cb45d4e9329d4b106cbd10c28622c3cb51bf0be3
ypraw <dyo.ndutz@gmail.com>
2021-04-12 12:24:57 +0700

    REF (STOWAPP) : Refactored Menu Screen

--- stowtui/stowApp.py
+++ stowtui/stowApp.py
@@ -15,11 +15,12 @@ class StowApp(npyscreen.NPSAppManaged):
 
         quit_s = '\t' * 2 + '| ^Q to Exit the Apps '
         about_s = '\t' + '| ^A to About Apps |'
+        mark_s = '\t' * 2 + '| ^S to help |'
         version = Version()
         name = app_name().capitalize()
 
         self.addForm(
             "MAIN",
             FileManagerTUI,
-            name=name + '\t' + version + quit_s + about_s,
+            name=name + '\t' + version + quit_s + about_s + mark_s,
         )
